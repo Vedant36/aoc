ff4055945931703a57def82c3645611be6ce32c7
Vedant36 <vedantneema@gmail.com>
2021-12-11 11:28:42 +0530

    added solutions for Dec 11 2021

--- 2021/11/part1.py
+++ 2021/11/part1.py
@@ -4,11 +4,31 @@ import numpy as np
 
 def filter_data():
 	with open('input', 'r') as fp:
-		data = fp.read().splitlines()
+		data = np.asarray([ list(i) for i in fp.read().splitlines() ], dtype=int)
 		return data
+
+def step(data_):
+    data = data_.copy()
+    data+=1
+    flashed = []
+    while np.sum(data>9)>len(flashed):
+        for n1,i in enumerate(data):
+            for n2,j in enumerate(i):
+                if j>9 and (n1,n2) not in flashed:
+                    data[n1-1 if n1 else 0:n1+2,n2-1 if n2 else 0:n2+2]+=1
+                    flashed.append((n1,n2))
+    for i in flashed:
+        data[i]=0
+    return data,len(flashed)
 
 def main():
 	data = filter_data()
+	a,temp = step(data)
+	flashes = temp
+	for i in range(99):
+		a,temp = step(a)
+		flashes+= temp
+	return flashes
 
 if __name__ == '__main__':
 	print(main())
--- 2021/11/part2.py
+++ 2021/11/part2.py
@@ -4,6 +4,13 @@ from part1 import *
 
 def main():
 	data = filter_data()
+	a = step(data)[0]
+	i=1
+	while True:
+		a = step(a)[0]
+		i+=1
+		if np.sum(a)==0:
+			return i
 
 if __name__ == '__main__':
 	print(main())
