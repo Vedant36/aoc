e0c73927dfd0ec55dbcce7c8c10ec4f097acf400
Vedant36 <vedantneema@gmail.com>
2021-12-12 16:43:14 +0530

    added Dec 12 2021 solutions

--- 2021/12/part1.py
+++ 2021/12/part1.py
@@ -5,10 +5,49 @@ import numpy as np
 def filter_data():
 	with open('input', 'r') as fp:
 		data = fp.read().splitlines()
-		return data
+		return [set(i.split('-')) for i in data]
 
+class Graph:
+	def __init__(self, connections):
+		self.connections = connections
+		a = []; [a.extend(list(i)) for i in self.connections]
+		self.uniq = set(a)
+
+	def connected(self, node:str):
+		qwe = set()
+		for i in self.connections:
+			if node in i:
+				qwe = qwe.union(i.difference({node}))
+		return qwe
+
+	def count(self):
+		cache = { i:self.connected(i) for i in self.uniq }
+		paths = []
+		next = cache['start']
+		for node in next:
+			paths.append(['start', node])
+		while not all([i[-1]=='end' for i in paths]):
+			temp = []
+			for path in paths:
+				if path[-1]=='end':
+					temp.append(path)
+					continue
+				next = cache[path[-1]]
+				for node in next:
+					if node.isupper() or node not in path:
+						temp2 = path.copy()
+						temp2.append(node)
+						temp.append(temp2)
+			paths = temp.copy()
+		return paths
+
+# [{'MN', 'yw'}, {'wn', 'XB'}, {'DG', 'dc'}, {'MN', 'wn'}, {'DG', 'yw'}, {'start', 'dc'}, {'start', 'ah'}, {'MN', 'start'}, {'fi', 'yw'}, {'fi', 'XB'}, {'wn', 'ah'}, {'MN', 'ah'}, {'MN', 'dc'}, {'yw', 'end'}, {'fi', 'end'}, {'th', 'fi'}, {'end', 'XB'}, {'dc', 'XB'}, {'yw', 'XN'}, {'yw', 'wn'}, {'dc', 'ah'}, {'MN', 'fi'}, {'DG', 'wn'}]
 def main():
 	data = filter_data()
+	graph = Graph(data)
+	print(graph.uniq)
+	return graph.count()
 
 if __name__ == '__main__':
-	print(main())
+	paths = main()
+	print(len(paths))
--- 2021/12/part2.py
+++ 2021/12/part2.py
@@ -2,8 +2,49 @@
 
 from part1 import *
 
+class Graph:
+	def __init__(self, connections):
+		self.connections = connections
+		a = []; [a.extend(list(i)) for i in self.connections]
+		self.uniq = set(a)
+
+	def connected(self, node:str):
+		qwe = set()
+		for i in self.connections:
+			if node in i:
+				qwe = qwe.union(i.difference({node}))
+		return qwe
+
+	def count(self):
+		cache = { i:self.connected(i) for i in self.uniq }
+		paths = []
+		next = cache['start']
+		for node in next:
+			paths.append(['start', node])
+		while not all([i[-1]=='end' for i in paths]):
+			temp = []
+			for path in paths:
+				if path[-1]=='end':
+					temp.append(path)
+					continue
+				next = cache[path[-1]]
+				condition = not any([path.count(i)-1 for i in path if i.islower()])
+				for node in next:
+					if node=='start': continue
+					if node.isupper() or (node not in path) or condition:
+						temp2 = path.copy()
+						temp2.append(node)
+						temp.append(temp2)
+			paths = temp.copy()
+		return paths
+
 def main():
 	data = filter_data()
+	graph = Graph(data)
+	print(graph.uniq)
+	return graph.count()
 
 if __name__ == '__main__':
-	print(main())
+	paths = main()
+	print(len(paths))
+	# print(paths)
