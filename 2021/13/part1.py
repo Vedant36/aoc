465d6f0e5e752d2ccca0f18b356164f62bec3c6d
Vedant36 <vedantneema@gmail.com>
2021-12-13 17:20:05 +0530

    added solutions for Dec 13 2021

--- 2021/13/part1.py
+++ 2021/13/part1.py
@@ -5,10 +5,30 @@ import numpy as np
 def filter_data():
 	with open('input', 'r') as fp:
 		data = fp.read().splitlines()
-		return data
+		poi, folds = data[:799], data[800:]
+		poi = np.asarray([ eval(i) for i in poi ], dtype=int)
+		return poi, folds
+
+def fold(poi_, axis, line):
+    poi = poi_.copy()
+    for i in poi:
+        if i[axis]>line:
+            i[axis] = 2*line-i[axis]
+    return poi
 
 def main():
-	data = filter_data()
+	poi, folds = filter_data()
+
+	a = poi.copy()
+	axis=0
+	if 'y' in folds[0]:
+		axis=1
+	line = int(folds[0].split('=')[1])
+	a = fold(a, axis, line)
+
+	k = set([tuple(i) for i in fold(a, 0, 655)])
+
+	return len(k)
 
 if __name__ == '__main__':
 	print(main())
--- 2021/13/part2.py
+++ 2021/13/part2.py
@@ -3,7 +3,28 @@
 from part1 import *
 
 def main():
-	data = filter_data()
+	poi, folds = filter_data()
+
+	a = poi.copy()
+	for i in folds[0:]:
+		axis=0
+		if 'y' in i:
+			axis=1
+		line = int(i.split('=')[1])
+		a = fold(a, axis, line)
+
+	ans = ''
+	min, max = a.min(0), a.max(0)
+	k = set([tuple(i) for i in fold(a, 0, 655)])
+	for i in range(min[1], max[1]+1):
+		for j in range(min[0], max[0]+1):
+			if (j,i) in k:
+				ans+='QQ'
+			else:
+				ans+='  '
+		ans+='\n'
+
+	return ans
 
 if __name__ == '__main__':
 	print(main())
