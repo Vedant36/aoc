16686f38065a0298bf333032a7a5629fcdb6577b
Vedant36 <vedantneema@gmail.com>
2021-12-15 21:32:13 +0530

    added solutions for Dec 15 2021

--- 2021/15/part1.py
+++ 2021/15/part1.py
@@ -4,11 +4,47 @@ import numpy as np
 
 def filter_data():
 	with open('input', 'r') as fp:
-		data = fp.read().splitlines()
+		data = np.asarray([list(i) for i in fp.read().splitlines()], dtype=int)
 		return data
+
+def Dijkstra(dataset, source, destination):
+	U = {} # unvisited
+	T = {}
+	for i in dataset:
+		U[i] = np.inf
+		T[i] = np.inf
+	U[source] = 0
+	T[source] = 0
+	I = {source} # intermediate
+	while U:
+		min = np.inf
+		for i in I:
+			if U[i]<min:
+				min = U[i]
+				C = i
+		for i in [(C[0]-1,C[1]),(C[0],C[1]-1),(C[0]+1,C[1]),(C[0],C[1]+1)]:
+			if i in U:
+				I.add(i)
+				temp = U[C] + dataset[i]
+				if U[i]>temp:
+					U[i] = temp
+					T[i] = temp
+		U.pop(C)
+		I.discard(C)
+	return T
 
 def main():
 	data = filter_data()
+	
+	dataset = {}
+	for n1,i in enumerate(data):
+		for n2,j in enumerate(i):
+			dataset[(n1,n2)] = j
+	destination = (data.shape[0]-1,data.shape[1]-1)
+	del data
+	a = Dijkstra(dataset, (0,0), destination)
+	return a[destination]
 
 if __name__ == '__main__':
 	print(main())
+
--- 2021/15/part2.py
+++ 2021/15/part2.py
@@ -4,6 +4,26 @@ from part1 import *
 
 def main():
 	data = filter_data()
+	a = data.copy()
+	a = np.r_[a,(a)%9+1,(a+1)%9+1,(a+2)%9+1,(a+3)%9+1]
+	a = np.c_[a,(a)%9+1,(a+1)%9+1,(a+2)%9+1,(a+3)%9+1]
+	data = a.copy()
+	
+	dataset = {}
+	for n1,i in enumerate(data):
+		for n2,j in enumerate(i):
+			dataset[(n1,n2)] = j
+	destination = (data.shape[0]-1,data.shape[1]-1)
+	del data
+	a = Dijkstra(dataset, (0,0), destination)
+	return a[destination]
 
 if __name__ == '__main__':
-	print(main())
+	import cProfile
+	import pstats
+
+	with cProfile.Profile() as pr:
+		print(main())
+	stats = pstats.Stats(pr)
+	stats.sort_stats(pstats.SortKey.TIME)
+	stats.print_stats()
